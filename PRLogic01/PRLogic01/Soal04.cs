4e644e95489116137bcae852c64f08ea37c53952
dearmhmdr <muhammadddear@gmail.com>
2019-01-15 22:01:04 +0700

    PR soal Nomor 4 dan 5

---
+++ PRLogic01/PRLogic01/Soal04.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam01
+{
+    class Soal04
+    {
+        public Soal04()
+        {
+            Console.Write("Masukkan Nilai n: ");
+            int n = int.Parse(Console.ReadLine());
+            //baris
+            for (int i = 0; i < n; i++)
+            {
+                //kolom
+                for (int j = 0; j < n; j++)
+                {
+                    //Pindah Kolom
+                    if (i == j || i + j == n - 1||j==(n-1)/2||i== (n - 1) / 2)
+                    {
+                        //Console.Write("\t{0}|{1}", i, j);
+                        Console.Write("*\t");
+                    }
+                    else
+                    {
+                        Console.Write("\t");
+                    }
+                }
+                //pindah baris
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PRLogic01/PRLogic01/Soal05.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam01
+{
+    class Soal05
+    {
+        public Soal05()
+        {
+            Console.Write("Masukkan Nilai n: ");
+            int n = int.Parse(Console.ReadLine());
+            //baris
+            for (int i = 0; i < n; i++)
+            {
+                //kolom
+                for (int j = 0; j < n; j++)
+                {
+                    //Pindah Kolom
+                    if (i == j || i + j == n - 1||i==0||j==0||i==n-1||j==n-1)
+                    {
+                        //Console.Write("\t{0}|{1}", i, j);
+                        Console.Write("*\t");
+                    }
+                    else
+                    {
+                        Console.Write("\t");
+                    }
+                }
+                //pindah baris
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
