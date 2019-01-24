a6bbf78a8d00db2d4d7b10d9232b7cbbd7133e89
dearmhmdr <muhammadddear@gmail.com>
2019-01-24 10:18:11 +0700

    tugas PR2

---
+++ Exam01/Exam06/PR2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExamBase;
+
+
+namespace Exam06
+{
+    class PR2:LogicBase
+    {
+        public PR2(int n)
+        {
+            JmlhBaris = 3 + (n - 1);
+            JmlhKolom = (n * 3 + (n * n - 1) / 2);
+            Array2D = new String[JmlhBaris, JmlhKolom];
+            IsiArray(n);
+            FunctionBase.CetakArray(Array2D);
+        }
+
+        private void IsiArray(int n)
+        {
+            int angka = 18;
+            int deret = 18;
+            for (int bgn = 0; bgn < n; bgn++)
+            {
+                int stBrs = bgn;
+                int enBrs = n + 1;
+                int stKlm = (bgn * 6) - (bgn * (bgn - 1) / 2);
+                int enKlm = stKlm + 5 - bgn;
+                for (int b = stBrs; b <= enBrs; b++)
+                {
+                    angka = deret;
+                    for (int k = stKlm; k <= enKlm; k++)
+                    {
+                        if (b+k==stKlm+enBrs||b==stBrs||k==stKlm)
+                        {
+                            Array2D[b, k] = angka.ToString();
+                        }
+                        angka--;
+                    }
+                }
+                deret = angka;
+            }
+        }
+    }
+}
